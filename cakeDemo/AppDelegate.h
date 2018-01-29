588f0aa8380e33ee74d9a8a38487e932396ef4f1
Moses <toby@MosesdeiMac-2.local>
2018-01-29 13:18:39 +0800

    Initial Commit

---
+++ cakeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cakeDemo
+//
+//  Created by Toby on 2018/1/29.
+//  Copyright © 2018年 Toby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
